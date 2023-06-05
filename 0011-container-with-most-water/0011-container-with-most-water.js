c7080f85372aa9c12cffc160e48afb75394cd71a
Ichinnorov Tuguldur <t_ichka@yahoo.com>
2023-06-05 17:38:59 -0500

    Time: 101 ms (7.06%), Space: 58.4 MB (5.02%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,19 @@
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length-1;
+    let max = [];
+    let area = 0;
+    
+    for(var i=left; i<height.length; i++){
+        let distance = right-left;
+        area = Math.min(height[left],height[right])*distance;
+        max.push(area);
+        if(height[left]<=height[right]){
+            left++;
+        }else{
+            right--;
+        }
+    }
+    console.log("max:", max);
+    return Math.max(...max);
+};
