f8a4b5eca6c6cc20cb8f14c46a6af22d28311408
Ichinnorov Tuguldur <t_ichka@yahoo.com>
2023-07-03 13:15:48 -0500

    Time: 804 ms (5.07%), Space: 118.1 MB (5.07%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
@@ -0,0 +1,51 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {boolean}
+ */
+var closeStrings = function(word1, word2) {
+    if(word2.length !== word1.length){
+        return false;
+    }
+    
+    let wordMap =  new Map();
+    for(let e in word1){
+        if(!wordMap.has(word1[e])){
+            wordMap.set(word1[e],1);
+        }else{
+            wordMap.set(word1[e],wordMap.get(word1[e])+1);
+        }
+    }
+    
+    let wordMap2 =  new Map();
+    for(let e in word1){
+        if(!wordMap2.has(word2[e])){
+            wordMap2.set(word2[e],1);
+        }else{
+            wordMap2.set(word2[e],wordMap2.get(word2[e])+1);
+        }
+    }
+    
+    let occurences = [];
+    
+    for(let [key,value] of wordMap){
+        occurences.push(value);
+    }
+    
+    for(let e in word2){
+        // console.log(wordMap2.get(word2[e]));
+        if(occurences.includes(wordMap2.get(word2[e])) && wordMap.has(word2[e])){
+            let index = occurences.indexOf(wordMap2.get(word2[e]));
+            occurences.splice(index,1);
+            wordMap2.delete(word2[e]);
+        };
+    }
+    if(wordMap2.size == 0){
+        return true;
+    }
+    return false;
+};
+
+
+
+
