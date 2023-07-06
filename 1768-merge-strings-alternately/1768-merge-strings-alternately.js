f4ddbdf9a0c63b80e91c528e518ffd0adb1be5a8
Ichinnorov Tuguldur <t_ichka@yahoo.com>
2023-07-06 15:10:52 -0500

    Time: 65 ms (28.58%), Space: 42.3 MB (56.15%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    let answer = "";
+    
+    for(let i=0; i<word1.length; i++){
+        if(word1[i] !== undefined && word2[i] !== undefined){
+            answer += word1[i] + word2[i];
+        }else{
+          break;
+        }
+    }
+    
+    if(word1.length > word2.length){
+        let len = word1.length - word2.length;
+        answer += word1.slice(word1.length - len, word1.length);
+    }else{
+        let len = word2.length - word1.length;
+        answer += word2.slice(word2.length - len, word2.length);
+    }
+    return answer;
+};
