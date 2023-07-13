0fa33611ce4d38bda2b8fda0d9c9924ff554c2ec
Ichinnorov Tuguldur <t_ichka@yahoo.com>
2023-07-13 14:16:12 -0500

    Time: 97 ms (26.69%), Space: 50.3 MB (41.86%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -2,20 +2,24 @@
  * @param {string} s
  * @return {string}
  */
-var reverseVowels = function(s) {
-    let arr = s.split("");
+var reverseVowels = function(s) {  
     let vowels = ['a','e','i','o','u','A','E','I','O','U',];
-    let elements = [];
-    for(let i=0; i<s.length; i++){
-        if(vowels.includes(s[i])){
-            elements.push(s[i]);
+    let start = 0;
+    let end = s.length-1;
+    let arr = s.split("");
+    
+    while(start < end){
+        if(!vowels.includes(s[start])){
+            start++;
         }
-    }
-
-    for(let j=0; j<s.length; j++){
-        if(vowels.includes(s[j])){
-            arr.splice(j,1,elements[elements.length-1]);
-            elements.pop();
+        if(!vowels.includes(s[end])){
+            end--;
+        }
+        if(vowels.includes(s[start]) && vowels.includes(s[end])){
+            arr.splice(start,1,s[end]);
+            arr.splice(end,1,s[start]);
+            start++;
+            end--;
         }
     }
     s = arr.join("");
